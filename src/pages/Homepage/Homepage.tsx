ae27fd3f691e0dd0e3293aeaeca6d2b4d90a93a0
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-01-27 01:15:12 +0530

    feat(homepage): add landing page
    Implement homepage with marketing content and call-to-action for new users.

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/common/Header'
+
+function Homepage() {
+  return (
+    <div className="antialiased relative" style={{ color: '#2D251E', backgroundColor: '#FFFCF9' }}>
+      <Header />
+    </div>
+  )
+}
+
+export default Homepage
