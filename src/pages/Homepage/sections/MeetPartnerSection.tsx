28ddd9b203e5359180c440803dfd9dd2574e621b
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-01-29 03:17:37 +0530

    chore(client): remove unused Sparkles icon import

--- src/pages/Homepage/sections/MeetPartnerSection.tsx
+++ src/pages/Homepage/sections/MeetPartnerSection.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { ArrowRight, Heart, Sparkles } from 'lucide-react'
+import { ArrowRight, Heart } from 'lucide-react'
 
 export default function MeetPartnerSection() {
   return (
